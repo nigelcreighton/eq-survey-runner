39655e4e4f463926345d78a79c59e4ec06bfb22f
Richard Fortescue-Webb <rfwebb@gmail.com>
2019-04-17 10:40:19 +0100

    Use a dataclass for Location

--- app/questionnaire/location.py
+++ app/questionnaire/location.py
@@ -1,30 +1,21 @@
 from __future__ import annotations
+from dataclasses import dataclass
 
 from flask import url_for
 
-
+@dataclass
 class Location:
+    """
+        Store a location in the questionnaire.
 
-    def __init__(self, block_id: str, list_name: str = None, list_item_id: str = None):
-        """
-        Args:
-            block_id: The id of the current block. This could be a block inside a list collector
-            list_item_id: The list_item_id if this location is associated with a list
-            list_name: The list name
-        """
-        self.block_id = block_id
-        self.list_name = list_name
-        self.list_item_id = list_item_id
+        block_id: The id of the current block. This could be a block inside a list collector
+        list_item_id: The list_item_id if this location is associated with a list
+        list_name: The list name
+    """
 
-    def __eq__(self, other: Location):
-        """
-        Check to see if two locations are equal.
-        Two answers are considered to be equal if their dictionary representations equal one another.
-
-        :param other: An answer to compare
-        :return: True if both answers match, otherwise False.
-        """
-        return isinstance(other, Location) and self.__dict__ == other.__dict__
+    block_id: str
+    list_name: str = None
+    list_item_id: str = None
 
     def __hash__(self):
         return hash(frozenset(self.__dict__.values()))
@@ -36,14 +27,6 @@ class Location:
         :return:
         """
         return '{}'.format(self.block_id)
-
-    def __repr__(self):
-        """
-        String representation of the location, handy for debug messages
-
-        :return:
-        """
-        return str(self)
 
     @classmethod
     def from_dict(cls, location_dict: dict):
