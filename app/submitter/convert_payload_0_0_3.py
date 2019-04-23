35bece315fc25ea9ee08cb17dd8f2a2c340dd4a5
Richard Fortescue-Webb <rfwebb@gmail.com>
2019-04-23 10:25:46 +0100

    Update lockfile

---
+++ app/submitter/convert_payload_0_0_3.py
@@ -0,0 +1,23 @@
+def convert_answers_to_payload_0_0_3(answer_store, schema, routing_path):
+    """
+    Convert answers into the data format below
+    'data': [
+        {
+            'value': 'Joe Bloggs',
+            'answer_id': 'household-full-name',
+        },
+        {
+            'value': 'Husband or wife',
+            'answer_id': 'who-is-related',
+        }
+    ]
+    :param answer_store: questionnaire answers
+    :param routing_path: the path followed in the questionnaire
+    :return: data in a formatted form
+    """
+    data = []
+    for location in routing_path:
+        answer_ids = schema.get_answer_ids_for_block(location.block_id)
+        answers_in_block = answer_store.get_answers_by_answer_id(answer_ids, list_item_id=location.list_item_id)
+        data.extend(answers_in_block)
+    return data
